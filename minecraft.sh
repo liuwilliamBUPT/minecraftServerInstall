45e343b4116c2164eee45fa3c08a7f9938c76a5d
William <liuwilliam.zh@gmail.com>
2019-06-20 18:23:44 +0800

    添加了sudoExec函数用于执行sudo命令，添加了国家检测，用于是否使用ppa反代的检测，以及发现了一些bug

--- minecraft.sh
+++ minecraft.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This function is used to check whether the input is numbers.Accept string arguments and replace numbers with regulars.Then check whether the ${tmp} is null.
+# This function is used to check whether the input is numbers. Accept string arguments and replace numbers with regulars. Then check whether the ${tmp} is null.
 checkInt(){
 tmp=$(echo $1|sed 's/[0-9]//g')
 if [ -n "${tmp}" ]; then
@@ -10,23 +10,63 @@ else
 fi
 }
 
+# This function is used to execute sudo command.
+sudoExec(){
+if $sudo_flag; then
+	local cmd='sudo '
+	cmd+=$1
+	eval $cmd
+	return $?
+else
+	eval $1
+	return $?
+fi
+}
 
 echo 'Check installation status of java. Notice that this script will check openjdk-8-jdk-headless install status.'
 
+# Detect sudo status.
+if dpkg --get-selections | grep sudo ; then
+	sudo_flag=true
+else
+	sudo_flag=false
+fi
 
-#change to install jdk8 sudo apt install openjdk-8-jdk-headless
-#maybe change here to case.
+# change to install jdk8 sudo apt install openjdk-8-jdk-headless
+# launchpad.proxy.ustclug.org ppa.launchpad.net http://mirrors.ustc.edu.cn/
+# maybe change here to case.
+# sudo add-apt-repository ppa:webupd8team/java  sudo apt-get install software-properties-common python-software-properties add-apt-repository: command not found /etc/apt/sources.list
+# $ sudo add-apt-repository ppa:openjdk-r/ppa
+# $ sudo apt-get update
+# $ sudo apt-get install openjdk-8-jdk
+# sudo apt-get update
+geoip=$(curl -s https://api.ip.sb/geoip)
+if echo $ip | grep '"country_code":"CN"' >/dev/null 2>&1; then
+	CN=true
+else
+	CN=false
+fi
+
+checkPackage='apt search openjdk-8-jdk-headless | grep openjdk-8-jdk-headless'
+
+if ! sudoExec $checkPackage; then
+	apt_repo='apt-get install software-properties-common python-software-properties -y'
+	sudoExec $apt_repo
+	sudoExec 'add-apt-repository ppa:openjdk-r/ppa -y'
+	if CN; then
+		sed -i "s/ppa\\.launchpad\\.net/launchpad\\.proxy\\.ustclug\\.org/g" /etc/apt/sources.list.d/openjdk-r-ubuntu-ppa-*.list
+	fi
+fi
+
+sudoExec 'apt update'
+
 if dpkg --get-selections | grep openjdk-8-jdk-headless; then
 	echo "Package openjdk-8-jdk-headless have been installed."
 else
 	echo "Installing openjdk-8-jdk-headless... "
-	if ! sudo apt-get update 2>/dev/null && ! sudo apt-get install -y openjdk-8-jdk-headless 2>/dev/null; then
-	apt-get update && apt-get install -y openjdk-8-jdk-headless
+	sudoExec 'apt update && apt install -y openjdk-8-jdk-headless'
 	echo "Package openjdk-8-jdk-headless installed. "
-	fi
 fi
-
-#sudo may not install
 
 echo "Create minecraft directory."
 if [ ! -d ~/minecraft ]; then
@@ -36,12 +76,12 @@ fi
 cd minecraft || cd ~/minecraft || return 255
 if [ $? = 255 ]; then echo "No such Directory!";fi
 
-#Set a flag to detect whether to download a new minecraft server.
+# Set a flag to detect whether to download a new minecraft server.
 
 
 if [ -f ~/minecraft/minecraft_server.*.jar ]; then
 	tempver=$(find ~/minecraft/minecraft_server.*.jar | awk -F[\.] '{print $2"."$3"."$4}')
-	#Store the existed minecraft server version in tempver.
+	# Store the existed minecraft server version in tempver.
 	echo -n "Detect that there exists minecraft_server.${tempver}.jar, do you want to get a new one?(Y/n)[Default = N]:"
 	read judge
 	if [ -z $judge ];then
@@ -61,16 +101,16 @@ else
 	flag=1
 fi
 
-#handle flag.
+# handle flag.
 if [ $flag -eq 1 ]; then
 	echo -n "Chose the version(default = 1.12.2) you want to use:"
 	read version
 	if [ -z $version ];then
 		version='1.12.2'
 	fi
-	#dupe cd minecraft || cd ~/minecraft || return 255
-	#wget -O minecraft_server.jar https://s3.amazonaws.com/Minecraft.Download/versions/$version/minecraft_server.$version.jar
-	#wget https://s3.amazonaws.com/Minecraft.Download/versions/$version/minecraft_server.$version.jar
+	# dupe cd minecraft || cd ~/minecraft || return 255
+	# wget -O minecraft_server.jar https://s3.amazonaws.com/Minecraft.Download/versions/$version/minecraft_server.$version.jar
+	# wget https://s3.amazonaws.com/Minecraft.Download/versions/$version/minecraft_server.$version.jar
 	wget --header="Host: s3.amazonaws.com" \
 	--header="Connection: keep-alive" \
 	--header="Upgrade-Insecure-Requests: 1" \
@@ -83,14 +123,14 @@ fi
 
 # The above url may be invalid in the future.
 
-#Use loop to ensure the input strings are numeric.
+# Use loop to ensure the input strings are numeric.
 while true
 do
 	echo "Notice:You can use Ctrl + Backspace to delete error entry."
 	read -p "Set the minimum memory and maximum memory, example:1024 1024:" minmem maxmem
-	#If you make an input mistake, use Ctrl + Backspace to delete that.
-	#whether how much args this will return 0
-	#echo $maxmem $minmem
+	# If you make an input mistake, use Ctrl + Backspace to delete that.
+	# whether how much args this will return 0
+	# echo $maxmem $minmem
 	if [ -z $maxmem ]; then
 		check=0
 	elif [ -z $minmem ]; then
@@ -98,44 +138,44 @@ do
 	else
 		check=1
 	fi
-	#Use maxS and minS to monitor the maxmem and minmen input status.
+	# Use maxS and minS to monitor the maxmem and minmen input status.
 	checkInt $maxmem
 	maxS=$?
 	checkInt $minmem
 	minS=$?
-	#Problem is that 0 input or 1 input this loop will exit. This may fixed?
+	# Problem is that 0 input or 1 input this loop will exit. This may fixed?
 	if [[ $check -eq 0 ]]; then
 		echo "Please enter the memory setting!"
-		#if check is not equal to 0 
+		# if check is not equal to 0 
 	elif [[ $maxS -eq 0 || $minS -eq 0 ]]; then
-		#if maxS or minS 
+		# if maxS or minS 
 		echo "Please make sure you what you entered are numbers!"
 	else
 		break
 	fi
 done
-#Check the installation status of the expect.
+# Check the installation status of the expect.
 echo "Check the installation status of the expect."
 dpkg --get-selections | grep expect
 if [ $? -ne 0 ]; then
 	echo "Installing expect..."
-	sudo apt-get install -y expect 2>/dev/null
+	sudo apt install -y expect 2>/dev/null
 	if [ $? -ne 0 ]; then
 	echo "Installing expect..."
-	apt-get install -y expect
+	apt install -y expect
 	fi
 fi
 
-#The part below is still on testing.
-#First, to check if the gameInit.exp exists. If not, check eula.txt...
-#echo "First, to check if the gameInit.exp exists. If not, check eula.txt..."
-#echo -n "just for stop,ready to cat > ~/minecraft/gameInit.exp" nothing
-#read nothing
+# The part below is still on testing.
+# First, to check if the gameInit.exp exists. If not, check eula.txt...
+# echo "First, to check if the gameInit.exp exists. If not, check eula.txt..."
+# echo -n "just for stop,ready to cat > ~/minecraft/gameInit.exp" nothing
+# read nothing
 
-#Check if eual.txt exists.
+# Check if eual.txt exists.
 if [ ! -f ~/minecraft/eula.txt ]; then
-	#If not, create gameInit.exp to firstly launch mc, and this file will be created automatically.
-	#RTFM to learn how to use expect wisely.
+	# If not, create gameInit.exp to firstly launch mc, and this file will be created automatically.
+	# RTFM to learn how to use expect wisely.
 	cat > ~/minecraft/gameInit.exp<<EOF
 #!/usr/bin/expect -f
 set timeout 30
@@ -148,12 +188,12 @@ expect "*Stopping*" {exec sh -c {
 touch finised
 }}
 EOF
-	#echo '#! /usr/bin/expect -f
-	#puts aaa' >flagf.exp 
-	#success？
+	# echo '#! /usr/bin/expect -f
+	# puts aaa' >flagf.exp 
+	# success？
 	temp=$?
 	
-	#This may dosen't work?
+	# This may dosen't work?
 		
 	if [ $temp -eq 0 ]; then
 
@@ -163,13 +203,14 @@ EOF
 		sed -i 's/online-mode=true/online-mode=false/g' ~/minecraft/server.properties
 	fi	
 else
-	#Else check whether the files have been modified.
+	# Else check whether the files have been modified.
 	cat ~/minecraft/eula.txt | grep eula=true >/dev/null 2>&1 
 	if [ $? -eq 0 ];then
 		echo -n "Detect that there you might have run minecraft_server.${version}.jar successfully."
 	else
 		echo "Modify eula.txt and server.properties now."
 		sed -i 's/eula=false/eula=true/g' ~/minecraft/eula.txt
+		# 修改失效？
 		sed -i 's/online-mode=true/online-mode=false/g' ~/minecraft/server.properties
 		rm gameInit.exp
 	fi
@@ -200,18 +241,18 @@ do
 	sleep 1s
 done
 rm gameInit.exp finised
-#java -Xmx${maxmem}M -Xms${minmem}M -jar minecraft_server.jar nogui
-#if [ -z $version ];then
-#	$version='1.12.2'
-#fi
+# java -Xmx${maxmem}M -Xms${minmem}M -jar minecraft_server.jar nogui
+# if [ -z $version ];then
+# 	$version='1.12.2'
+# fi
 forgeversion="1.12.2-14.23.5.2812"
-#add set up 
+# add set up 
 if [ ! -f ~/minecraft/forge-*-universal.jar ];then
-	#wget -O forgeInstaller.jar https://files.minecraftforge.net/maven/net/minecraftforge/forge/$forgeversion/forge-$forgeversion-installer.jar
-	#java -jar forgeInstaller.jar nogui --installServer --offline
+	# wget -O forgeInstaller.jar https://files.minecraftforge.net/maven/net/minecraftforge/forge/$forgeversion/forge-$forgeversion-installer.jar
+	# java -jar forgeInstaller.jar nogui --installServer --offline
 	wget https://files.minecraftforge.net/maven/net/minecraftforge/forge/$forgeversion/forge-$forgeversion-installer.jar
 	java -jar forge-$forgeversion-installer.jar nogui --installServer
-	#rm forgeInstaller.jar forgeInstaller.jar.log
+	# rm forgeInstaller.jar forgeInstaller.jar.log
 fi
-
+sed -i 's/online-mode=true/online-mode=false/g' ~/minecraft/server.properties
 java -Xmx${maxmem}M -Xms${minmem}M -jar forge-$forgeversion-universal.jar nogui
